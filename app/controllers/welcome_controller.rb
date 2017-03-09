7cc2d158108370d29aeab26ecf5fbf2bceaa0d80
Tony <tangvwin@gmail.com>
2017-03-09 21:15:08 +0800

    install devise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,7 @@ class WelcomeController < ApplicationController
 
       def index
         flash[:notice] = "Good morning"
-        flash[:warning] = "Good afternoon"
-        flash[:alert] = "Good night"
+        
       end
 
 end
