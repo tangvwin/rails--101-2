ae3c9d56430285d110e08b66b7098d0881c03722
Tony <tangvwin@gmail.com>
2017-03-11 19:44:20 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,13 @@ class User < ApplicationRecord
        end
 
 
+       def join!(group)
+         participated_groups<<group
+       end
+
+       def quit!(group)
+         participated_groups.delete(group)
+       end
+
 
 end
